3d71e783943a44452dbd7fbe0874a199433ee215
Håkon Wiik Ånes <hwaanes@gmail.com>
2019-09-02 17:01:18 +0200

    Add test for set_phase_parameters() and test convenience function
    Signed-off-by: Håkon Wiik Ånes <hwaanes@gmail.com>

--- kikuchipy/tests/signals/test_ebsd.py
+++ kikuchipy/tests/signals/test_ebsd.py
@@ -22,6 +22,21 @@ import numpy as np
 import pytest
 
 
+def assert_dictionary(input_dict, output_dict):
+    for key in output_dict.keys():
+        if isinstance(output_dict[key], dict):
+            assert_dictionary(input_dict[key], output_dict[key])
+        else:
+            if isinstance(output_dict[key], list)\
+                    or isinstance(input_dict[key], list):
+                output_dict[key] = np.array(output_dict[key])
+                input_dict[key] = np.array(input_dict[key])
+            if isinstance(output_dict[key], np.ndarray):
+                assert input_dict[key].all() == output_dict[key].all()
+            else:
+                assert input_dict[key] == output_dict[key]
+
+
 class TestEBSD:
 
     def test_init(self):
@@ -37,8 +52,7 @@ class TestEBSD:
         assert array1.shape == s1.axes_manager.shape
 
     def test_set_experimental_parameters(self):
-        array = np.zeros(shape=(10, 10, 10, 10))
-        s = EBSD(array)
+        s = EBSD(np.zeros((10, 10, 10, 10)))
         p = {'detector': 'NORDIF UF-1100', 'azimuth_angle': 1.0,
              'elevation_angle': 1.0, 'sample_tilt': 70.0,
              'working_distance': 23.2, 'binning': 8, 'exposure_time': 0.01,
@@ -51,8 +65,26 @@ class TestEBSD:
         s.set_experimental_parameters(**p)
         ebsd_node = metadata_nodes(sem=False)
         md_dict = s.metadata.get_item(ebsd_node).as_dictionary()
-        for k, v in md_dict.items():
-            if isinstance(p[k], np.ndarray):
-                assert md_dict[k].all() == p[k].all()
-            else:
-                assert md_dict[k] == p[k]
+        assert_dictionary(p, md_dict)
+
+    def test_set_phase_parameters(self):
+        s = EBSD(np.zeros((10, 10, 10, 10)))
+        p = {'number': 1,
+             'atom_coordinates': {
+                 '1': {'atom': 'Ni',
+                       'coordinates': [0, 0, 0],
+                       'site_occupation': 1,
+                       'debye_waller_factor': 0.0035}},
+             'formula': 'Ni',
+             'info': 'Some sample info',
+             'lattice_constants': [0.35236, 0.35236, 0.35236, 90, 90, 90],
+             'laue_group': 'm3m',
+             'material_name': 'Ni',
+             'point_group': '432',
+             'space_group': 225,
+             'setting': 1,
+             'symmetry': 43}
+        s.set_phase_parameters(**p)
+        md_dict = s.metadata.get_item('Sample.Phases.1').as_dictionary()
+        p.pop('number')
+        assert_dictionary(p, md_dict)
