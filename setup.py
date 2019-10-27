9629865407d5e056ec0853bdad010bb4d10833c7
Håkon Wiik Ånes <hwaanes@gmail.com>
2019-10-27 22:45:49 +0100

    Fix bug in setup.py and "update" version
    Signed-off-by: Håkon Wiik Ånes <hwaanes@gmail.com>

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
         'Intended Audience :: Science/Research',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Natural Language :: English',
-        'Operation System :: OS Independent',
+        'Operating System :: OS Independent',
         'Topic :: Scientific/Engineering',
         'Topic :: Scientific/Engineering :: Physics',
     ],
