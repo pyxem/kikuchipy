d1af9c86e87cf70f1c8287c7d9e212dc82b811dc
Håkon Wiik Ånes <hwaanes@gmail.com>
2019-12-28 15:14:55 +0100

    Install with pip on macOS
    Signed-off-by: Håkon Wiik Ånes <hwaanes@gmail.com>

--- ci/travis_install_with_pip.sh
+++ ci/travis_install_with_pip.sh
@@ -1,3 +1,8 @@
 # Install package with pip
-pip install -U $DEPS $TEST_DEPS
-pip list installed
+if [[ $TRAVIS_OS_NAME =~ ^(osx|windows)$ ]]; then
+  pip3 install -U $DEPS $TEST_DEPS
+  pip3 list installed
+else # linux
+  pip install -U $DEPS $TEST_DEPS
+  pip list installed
+fi
