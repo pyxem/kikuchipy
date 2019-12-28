07960efc9ce7963ab5bac623184a81d7c898084b
Håkon Wiik Ånes <hwaanes@gmail.com>
2019-12-28 17:09:58 +0100

    Install python3 via choco on windows/pip
    Signed-off-by: Håkon Wiik Ånes <hwaanes@gmail.com>

--- ci/travis_install_with_pip.sh
+++ ci/travis_install_with_pip.sh
@@ -1,10 +1,15 @@
+# Install Python3
+if [ "$TRAVIS_OS_NAME" == windows ]; then
+  choco install -y python3 --version=3.7.5 --allow-downgrades
+fi
+
 # Check Python and pip version
-python --version
+python3 --version
 pip --version
 
 # Create virtual environment
-python -m pip install --upgrade virtualenv
-virtualenv -p python --system-site-packages $HOME/testenv
+python3 -m pip install --upgrade virtualenv
+virtualenv -p python3 --system-site-packages $HOME/testenv
 source $HOME/testenv/bin/activate
 
 # Install package with pip
