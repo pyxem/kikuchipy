b66f91c1ac821775df1f6d418d68656e5a9d47d6
Håkon Wiik Ånes <hwaanes@gmail.com>
2019-12-28 15:44:21 +0100

    Build package in virtualenv, also printing versions before install
    Signed-off-by: Håkon Wiik Ånes <hwaanes@gmail.com>

--- ci/travis_install_with_pip.sh
+++ ci/travis_install_with_pip.sh
@@ -1,8 +1,16 @@
+# Check Python and pip version
+python3 -V
+pip -V
+
+python3 -m pip install --upgrade virtualenv
+virtualenv -p python3 --system-site-packages $HOME/venv
+source $HOME/venv/bin/activate
+
 # Install package with pip
-if [[ $TRAVIS_OS_NAME =~ ^(osx|windows)$ ]]; then
-  pip3 install -U $DEPS $TEST_DEPS
-  pip3 list installed
-else # linux
-  pip install -U $DEPS $TEST_DEPS
-  pip list installed
-fi
+#if [[ $TRAVIS_OS_NAME =~ ^(osx|windows)$ ]]; then
+#  pip3 install -U $DEPS $TEST_DEPS
+#  pip3 list installed
+#else # linux
+pip install --upgrade $DEPS $TEST_DEPS
+pip list installed
+#fi
