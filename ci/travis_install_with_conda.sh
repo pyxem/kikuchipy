51941c397f5639e87fc5792ed1fe97d9f6945d5c
Håkon Wiik Ånes <hwaanes@gmail.com>
2019-12-28 14:44:54 +0100

    Fix use of bash in before_install (missed newline)
    Signed-off-by: Håkon Wiik Ånes <hwaanes@gmail.com>

--- ci/travis_install_with_conda.sh
+++ ci/travis_install_with_conda.sh
@@ -1,5 +1,5 @@
 # Configure conda
-if [[ "$TRAVIS_OS_NAME" =~ ^(linux|osx)$ ]]; then
+if [[ $TRAVIS_OS_NAME =~ ^(linux|osx)$ ]]; then
   source $HOME/miniconda/bin/activate root
 fi
 
@@ -7,7 +7,7 @@ conda update --yes conda
 conda config --append channels conda-forge
 conda create --name testenv --yes python=$PYTHON_CONDA
 
-if [[ "$TRAVIS_OS_NAME" =~ ^(linux|osx)$ ]]; then
+if [[ $TRAVIS_OS_NAME =~ ^(linux|osx)$ ]]; then
   conda activate testenv
 else # windows
   . activate testenv
