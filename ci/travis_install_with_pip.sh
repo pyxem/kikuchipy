fb63bf491696ac4695332370b056d594dd214335
Håkon Wiik Ånes <hwaanes@gmail.com>
2019-12-28 16:58:22 +0100

    Try to fix unknown windows/pip error...
    Signed-off-by: Håkon Wiik Ånes <hwaanes@gmail.com>

--- ci/travis_install_with_pip.sh
+++ ci/travis_install_with_pip.sh
@@ -1,10 +1,10 @@
 # Check Python and pip version
-python3 --version
+python --version
 pip --version
 
 # Create virtual environment
-python3 -m pip install --upgrade virtualenv
-virtualenv -p python3 --system-site-packages $HOME/testenv
+python -m pip install --upgrade virtualenv
+virtualenv -p python --system-site-packages $HOME/testenv
 source $HOME/testenv/bin/activate
 
 # Install package with pip
