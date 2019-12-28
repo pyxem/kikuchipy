3dfff97a051fe253a5caef454e6852c0919b5b6e
Håkon Wiik Ånes <hwaanes@gmail.com>
2019-12-28 17:33:24 +0100

    Try python instead of python3 in windows/pip
    Signed-off-by: Håkon Wiik Ånes <hwaanes@gmail.com>

--- ci/travis_install_with_pip.sh
+++ ci/travis_install_with_pip.sh
@@ -1,17 +1,32 @@
-# Install Python3
-#if [ "$TRAVIS_OS_NAME" == windows ]; then
-#  choco install -y python3 --version=3.7.5 --allow-downgrades
-#fi
+# According to
+# https://github.com/cclauss/Travis-CI-Python-on-three-OSes/blob/master/.travis.yml:
+# 'python' points to Python 2.7 on macOS, but points to Python 3.7 on Linux and Windows
+# 'python3' is a 'command not found' error on Windows, but 'python' works on Windows only
+
+# Install Python3 on Windows
+if [ "$TRAVIS_OS_NAME" == windows ]; then
+  choco install -y python --version=3.7.5 --allow-downgrades
+  python -m pip install --upgrade pip
+fi
 
 # Check Python and pip version
-python3 --version
-pip --version
+if [[ "$TRAVIS_OS_NAME" =~ ^(linux|osx)$ ]]; then
+  python3 --version
+else # windows
+  python --version
+fi
+pip3 --version
 
 # Create virtual environment
-python3 -m pip install --upgrade virtualenv
-virtualenv -p python3 --system-site-packages $HOME/testenv
+if [[ "$TRAVIS_OS_NAME" =~ ^(linux|osx)$ ]]; then
+  python3 -m pip3 install --upgrade virtualenv
+  virtualenv -p python3 --system-site-packages $HOME/testenv
+else # windows
+  python -m pip3 install --upgrade virtualenv
+  virtualenv -p python --system-site-packages $HOME/testenv
+fi
 source $HOME/testenv/bin/activate
 
 # Install package with pip
-pip install --upgrade $DEPS $TEST_DEPS
-pip list installed
+pip3 install --upgrade $DEPS $TEST_DEPS
+pip3 list installed
