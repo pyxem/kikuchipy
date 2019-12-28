0c7a37a6b9f8d77fd8492babe132cbbadf40aba5
Håkon Wiik Ånes <hwaanes@gmail.com>
2019-12-28 16:00:59 +0100

    Install with pip on both Linux and macOS
    Signed-off-by: Håkon Wiik Ånes <hwaanes@gmail.com>

--- ci/travis_install_with_pip.sh
+++ ci/travis_install_with_pip.sh
@@ -1,16 +1,12 @@
 # Check Python and pip version
-python3 -V
-pip -V
+python3 --version
+pip --version
 
+# Create virtual environment
 python3 -m pip install --upgrade virtualenv
-virtualenv -p python3 --system-site-packages $HOME/venv
-source $HOME/venv/bin/activate
+virtualenv -p python3 --system-site-packages $HOME/testenv
+source $HOME/testenv/bin/activate
 
 # Install package with pip
-#if [[ $TRAVIS_OS_NAME =~ ^(osx|windows)$ ]]; then
-#  pip3 install -U $DEPS $TEST_DEPS
-#  pip3 list installed
-#else # linux
 pip install --upgrade $DEPS $TEST_DEPS
 pip list installed
-#fi
