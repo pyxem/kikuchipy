813f766244968738447cc52e356d7c837dbed162
Håkon Wiik Ånes <hwaanes@gmail.com>
2020-05-11 17:21:20 +0200

    Update Travis CI with stuff from master...
    Signed-off-by: Håkon Wiik Ånes <hwaanes@gmail.com>

--- ci/travis_setup_conda_osx.sh
+++ ci/travis_setup_conda_osx.sh
@@ -1,4 +1,6 @@
+sudo Xvfb :99 -ac -screen 0 1024x768x8 &
+sleep 1 # give xvfb some time to start
+
 # Install Miniconda
-curl https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -o miniconda.sh
-chmod +x miniconda.sh
-./miniconda.sh -b -p $HOME/miniconda
+wget https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh
+bash miniconda.sh -b -p $HOME/miniconda
