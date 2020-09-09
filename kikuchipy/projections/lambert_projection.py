fc269451fa1d89e17f3c09fc375053c7439394bb
Lars Lervik <friedkitteh@gmail.com>
2020-09-09 18:50:09 +0200

    "to lambert" equations should now be in place. Brain needed to decide which path to take still TODO.

--- kikuchipy/projections/lambert_projection.py
+++ kikuchipy/projections/lambert_projection.py
@@ -37,11 +37,32 @@ from kikuchipy.projections.spherical_projection import SphericalProjection
 class LambertProjection(SphericalProjection):
     """Lambert Projection of a vector (as implemented in MTEX?)"""
 
-    def project(self, v: Union[Vector3d, np.ndarray]) -> Vector3d:
-        # TODO: Implement equation 10a and 10b from Patrick G. Callahan and Marc De Graef
-        pass
+    def project(self, v: Union[Vector3d, np.ndarray]) -> np.ndarray:
+
+        if isinstance(v, Vector3d):
+            x = v.x
+            y = v.y
+            z = v.z
+        else:
+            x = v[..., 0]
+            y = v[..., 1]
+            z = v[..., 2]
+
+        # TODO: Implement requirement checker for which equation to use
+        something_important = True
+        if something_important:
+            # Equation 10a - Requirement: |y| <= |x|
+            X = np.sign(x)*np.sqrt(2*(1-z))*((np.sqrt(np.pi))/2)
+            Y = np.sign(x)*np.sqrt(2*(1-z))*((2/(np.sqrt(np.pi))) * np.arctan(y/x))
+        else:
+            # Equation 10b - Requirement: |x| <= |y|
+            X = np.sign(y)*np.sqrt(2*(1-z))*((2/(np.sqrt(np.pi))) * np.arctan(x/y))
+            Y = np.sign(y)*np.sqrt(2*(1-z))*((np.sqrt(np.pi))/2)
+
+        return np.column_stack((X, Y))
 
     @staticmethod
     def iproject(x: np.ndarray, y: np.ndarray) -> Vector3d:
-        # TODO: Implement equation 8a and 8b from Patrick G. Callahan and Marc De Graef
+        # TODO: Implement equation 8a from Patrick G. Callahan and Marc De Graef
+        # TODO: Implement equation 8b from Patrick G. Callahan and Marc De Graef
         pass
