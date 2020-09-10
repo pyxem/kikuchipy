770c4ec901a587cd0d40a3855afa60ac3405650d
Lars Lervik <friedkitteh@gmail.com>
2020-09-10 17:00:03 +0200

    Moved/Deleted some inline comments. Added some TODOs {skip CI}

--- kikuchipy/projections/lambert_projection.py
+++ kikuchipy/projections/lambert_projection.py
@@ -22,21 +22,8 @@ from orix.vector import Vector3d
 from kikuchipy.projections.gnomonic_projection import GnomonicProjection
 
 
-# This may not be needed? Dont think the inherit is needed
-from kikuchipy.projections.spherical_projection import SphericalProjection
-
-# Temporary notes for myself :)
-# Bunge Euler triplets
-# phi1, Phi, phi2
-
-# Pattern center
-# 2D coordinates = xpc, ypc
-
-# All coordinates in units of micrometers
-
-
-class LambertProjection(SphericalProjection):
-    """Lambert Projection of a vector (as implemented in MTEX?)"""
+class LambertProjection:
+    """Lambert Projection of a vector as implemented in MTEX"""
 
     @classmethod
     def project(self, v: Union[Vector3d, np.ndarray]) -> np.ndarray:
@@ -49,6 +36,9 @@ class LambertProjection(SphericalProjection):
             x = v[..., 0]
             y = v[..., 1]
             z = v[..., 2]
+
+        # TODO: This should be converted into numpy arrays from the get-go using np.where() - But I am still looking for
+        #  a way to make that work
 
         #  Use temporary lists for speed / memory
         li_X = [0]
@@ -87,17 +77,16 @@ class LambertProjection(SphericalProjection):
 
     @staticmethod
     def iproject(x: np.ndarray, y: np.ndarray) -> Vector3d:
-        X = x  # v[..., 0] The user needs to input this array?
-        Y = y  # v[..., 1] The user needs to input this array?
-        # A perhaps easier solution would be to take in **ONE** np.ndarray v and set X = v[..., 0], Y = v[..., 1]?
-
-        #  Use temporary lists for speed / memory
-        li_x = [0]
-        li_y = [0]
-        li_z = [0]
+        X = x
+        Y = y
 
         # TODO: This should be converted into numpy arrays from the get-go using np.where() - But I am still looking for
         #  a way to make that work
+
+        #  Use temporary lists, ideally would be numpy arrays from the start. Still finding implementation
+        li_x = [0]
+        li_y = [0]
+        li_z = [0]
 
         for x_val, y_val in zip(X, Y):
             # Edge cases - !! UNVERIFIED EQUATIONS !!
@@ -113,7 +102,6 @@ class LambertProjection(SphericalProjection):
                 li_x.append(_eq_c(x_val))
                 li_y.append(0)
                 li_z.append(li_z.append(1 - (2 * (x_val ** 2)) / np.pi))
-
             elif abs(y_val) <= abs(x_val):
                 # 0 < |Y| <= |X| <= L - Equation 8a
                 li_x.append(
@@ -133,7 +121,7 @@ class LambertProjection(SphericalProjection):
                 )
                 li_z.append(1 - (2 * (y_val ** 2)) / np.pi)
 
-        # Done appending, now we can use numpy arrays
+        # Done appending, convert to numpy array. Naive solution, probably a faster one out there
         x = np.array(li_x[1::])
         y = np.array(li_y[1::])
         z = np.array(li_z[1::])
@@ -148,18 +136,12 @@ def _eq_c(p: np.ndarray) -> np.ndarray:
 
 
 def lambert_to_gnomonic(v: np.ndarray) -> np.ndarray:
-    # Take vector from Lambert and convert it into an Orix Vector3d object
     vec = LambertProjection.iproject(v[..., 0], v[..., 1])
-    # This requires GnomonicProjection.project to be a class method, was prev. instance method - is that ok?
-    # Take Orix Vector 3d object and convert it to Gnomonic projection
     vec = GnomonicProjection.project(vec)
     return vec
 
 
 def gnomonic_to_lambert(v: np.ndarray) -> np.ndarray:
-    # Take vector from Gnomonic and convert it into an Orix Vector3d object
     vec = GnomonicProjection.iproject(v[..., 0], v[..., 1])
-    # This requires LambertProjection.project to be a class method, was prev. instance methd - is that ok?
-    # Take Orix Vector 3d object and convert it to Lambert projection
     vec = LambertProjection.project(vec)
     return vec
