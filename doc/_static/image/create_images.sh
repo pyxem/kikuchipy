168f990031901121261492d30431967e301eac80
Håkon Wiik Ånes <hwaanes@gmail.com>
2021-01-12 18:55:36 +0100

    Complete reference frame notebook (#283)
    Signed-off-by: Håkon Wiik Ånes <hwaanes@gmail.com>

--- doc/_static/image/create_images.sh
+++
@@ -1,8 +0,0 @@
-python doc_feature_maps_images.py
-python doc_processing_images.py
-python doc_reference_frames.py
-python doc_virtual_imaging.py
-python doc_visualize_images.py
-
-./concatenate_images.sh
-./remove_images_not_in_docs.sh
