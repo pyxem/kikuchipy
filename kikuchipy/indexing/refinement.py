d61f27ba1f7ab27110830ced7bb646447c879808
Lars Lervik <friedkitteh@gmail.com>
2021-02-17 07:59:24 +0100

    Created methods for single pattern simulations [skip CI]]

--- kikuchipy/indexing/refinement.py
+++ kikuchipy/indexing/refinement.py
@@ -6,8 +6,11 @@ import numpy as np
 from orix.quaternion import Rotation
 import scipy.optimize
 from kikuchipy.indexing.similarity_metrics import ncc
-from kikuchipy.signals import EBSDMasterPattern
-import graphviz
+from kikuchipy.signals import (
+    EBSDMasterPattern,
+    _get_direction_cosines,
+    _get_lambert_interpolation_parameters,
+)
 
 
 class Refinement:
@@ -29,14 +32,28 @@ class Refinement:
         rdata = xmap.rotations.data[:10]  # Smaller dataset for testing
         dtype = rdata.dtype
         # TODO: Optimize chunks, currently hardcoded for SDSS dataset
-        r_da = da.from_array(rdata, chunks=(11700, 1, 4))
-        mp_data = mp.data
+        # r_da = da.from_array(rdata, chunks=(11700, 1, 4))
+        r_da = da.from_array(rdata, chunks=(10, 1, 4))
+
+        (
+            master_north,
+            master_south,
+            dc,
+            npx,
+            npy,
+            scale,
+        ) = _get_single_pattern_params(mp, det, energy)
+
         exp_data = exp.data
         refined_params = r_da.map_blocks(
             _get_refined_params_chunk,
-            mp=mp_data,
+            master_north=master_north,
+            master_south=master_south,
+            dc=dc,
+            npx=npx,
+            npy=npy,
+            scale=scale,
             exp=exp_data,
-            energy=energy,
             det=det,
             pc_bounds=pc_bounds,
             degs=degs,
@@ -66,9 +83,13 @@ def _shgo_objective_function(x, *args):
     Phi = x[4]
     phi2 = x[5]
     experimental = args[0]
-    master_pattern_data = args[1]
-    detector = args[2]
-    energy = args[3]
+    master_north = args[1]
+    master_south = args[2]
+    dc = args[3]
+    npx = args[4]
+    npy = args[5]
+    scale = args[6]
+    detector = args[7]
     # We could probably speed this up quite a bit by using Bruker PC all the way
     # TODO: Use Bruker PC instead of EMsoft PC
     detector.pcx = 0.5 + (
@@ -77,19 +98,26 @@ def _shgo_objective_function(x, *args):
     detector.pcy = 0.5 - (ypc / (detector.nrows * detector.binning))
     detector.pcz = L / (detector.nrows * detector.px_size * detector.binning)
     r = Rotation.from_euler(np.radians(np.array((phi1, Phi, phi2))))
-    master_pattern = EBSDMasterPattern(
-        master_pattern_data,
-        projection="lambert",
-        hemisphere="both",
-        energy=energy,
+    sim_pattern = _simulate_single_pattern(
+        r, dc, master_north, master_south, npx, npy, scale
     )
-    # TODO: Create single pattern function that works on just arrays
-    sim_pattern = master_pattern.get_patterns(r, detector, energy, compute=True)
-    return -ncc(experimental, sim_pattern.inav[0].data)
+    return -ncc(experimental, sim_pattern)
 
 
 def _get_refined_params_chunk(
-    r, mp, exp, energy, det, pc_bounds, degs, ncols, dtype_out=np.float32
+    r,
+    master_north,
+    master_south,
+    dc,
+    npx,
+    npy,
+    scale,
+    exp,
+    det,
+    pc_bounds,
+    degs,
+    ncols,
+    dtype_out=np.float32,
 ):
     rotations = Rotation(r)
     rotations_shape = r.shape
@@ -114,7 +142,7 @@ def _get_refined_params_chunk(
         ]
         # Could probably speed up here as well
         bounds = pc_bounds + rotation_bounds
-        args = (exp_data, mp, det, energy)
+        args = (exp_data, master_north, master_south, dc, npx, npy, scale, det)
         optimized = scipy.optimize.shgo(
             _shgo_objective_function, bounds, args=args
         )
@@ -126,3 +154,82 @@ def _get_refined_params_chunk(
         phi2 = optimized.x[5]
         refined_params[index] = np.array((xpc, ypc, zpc, phi1, Phi, phi2))
     return refined_params
+
+
+def _get_single_pattern_params(mp, detector, energy):
+    # This method is already a part of the EBSDMasterPattern.get_patterns so
+    # it could probably replace it?
+    if mp.projection != "lambert":
+        raise NotImplementedError(
+            "Master pattern must be in the square Lambert projection"
+        )
+    if len(detector.pc) > 1:
+        raise NotImplementedError(
+            "Detector must have exactly one projection center"
+        )
+
+    # Get the master pattern arrays created by a desired energy
+    north_slice = ()
+    if "energy" in [i.name for i in mp.axes_manager.navigation_axes]:
+        energies = mp.axes_manager["energy"].axis
+        north_slice += ((np.abs(energies - energy)).argmin(),)
+    south_slice = north_slice
+    if mp.hemisphere == "both":
+        north_slice = (0,) + north_slice
+        south_slice = (1,) + south_slice
+    elif not mp.phase.point_group.contains_inversion:
+        raise AttributeError(
+            "For crystals of point groups without inversion symmetry, like "
+            f"the current {mp.phase.point_group.name}, both hemispheres "
+            "must be present in the master pattern signal"
+        )
+    master_north = mp.data[north_slice]
+    master_south = mp.data[south_slice]
+    dc = _get_direction_cosines(detector)
+    npx, npy = mp.axes_manager.signal_shape
+    scale = (npx - 1) / 2
+
+    return master_north, master_south, dc, npx, npy, scale
+
+
+def _simulate_single_pattern(
+    rotation,
+    dc,
+    master_north,
+    master_south,
+    npx,
+    npy,
+    scale,
+):
+    rotated_dc = rotation * dc
+    (
+        nii,
+        nij,
+        niip,
+        nijp,
+        di,
+        dj,
+        dim,
+        djm,
+    ) = _get_lambert_interpolation_parameters(
+        rotated_direction_cosines=rotated_dc,
+        npx=npx,
+        npy=npy,
+        scale=scale,
+    )
+    pattern = np.where(
+        rotated_dc.z >= 0,
+        (
+            master_north[nii, nij] * dim * djm
+            + master_north[niip, nij] * di * djm
+            + master_north[nii, nijp] * dim * dj
+            + master_north[niip, nijp] * di * dj
+        ),
+        (
+            master_south[nii, nij] * dim * djm
+            + master_south[niip, nij] * di * djm
+            + master_south[nii, nijp] * dim * dj
+            + master_south[niip, nijp] * di * dj
+        ),
+    )
+    return pattern
