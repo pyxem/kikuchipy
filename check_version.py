c3349a9d699eefbcbf0dd952d2843ba9e8657a0f
Håkon Wiik Ånes <hwaanes@gmail.com>
2021-09-01 11:18:27 +0200

    rc11

--- check_version.py
+++ check_version.py
@@ -7,21 +7,14 @@ from outdated import check_outdated
 with open("kikuchipy/release.py") as fid:
     for line in fid:
         if line.startswith("version"):
-            VERSION = line.strip().split(" = ")[-1][1:-1]
+            branch_version = line.strip().split(" = ")[-1][1:-1]
 
 try:
-    is_outdated, latest_version = check_outdated("kikuchipy", VERSION)
+    make_release, pypi_version = check_outdated("kikuchipy", branch_version)
 except ValueError as e:
-    latest_version = re.findall(r"\s([\d.]+)", e.args[0])[1]
+    pypi_version = re.findall(r"\s([\d.]+)", e.args[0])[1]
     is_outdated = True
 
-print(
-    f"Outdated:\t\t{is_outdated}\n"
-    f"PyPI version:\t\t{latest_version}\n"
-    f"'main' branch version:\t{VERSION}"
-)
-
-if is_outdated:
-    sys.exit(0)
-else:
-    sys.exit(1)
+print(make_release)
+print(pypi_version)
+print(branch_version)
