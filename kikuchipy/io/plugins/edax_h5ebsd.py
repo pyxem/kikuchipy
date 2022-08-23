3f0774187b7038c7884df0c540ab8b334e166d26
Håkon Wiik Ånes <hwaanes@gmail.com>
2022-08-23 13:56:53 +0200

    New EDAX TSL h5ebsd file reader
    Signed-off-by: Håkon Wiik Ånes <hwaanes@gmail.com>

--- kikuchipy/io/plugins/edax_h5ebsd.py
+++ kikuchipy/io/plugins/edax_h5ebsd.py
@@ -15,17 +15,232 @@
 # You should have received a copy of the GNU General Public License
 # along with kikuchipy. If not, see <http://www.gnu.org/licenses/>.
 
-"""Reader of EBSD data from a EDAX TSL h5ebsd file."""
+"""Reader of EBSD data from an EDAX TSL h5ebsd file."""
 
-from kikuchipy.io.plugins._h5ebsd import H5EBSDFile
+from pathlib import Path
+from typing import List, Optional, Union
+import warnings
+
+import dask.array as da
+import h5py
+import numpy as np
+from orix.crystal_map import CrystalMap
+
+from kikuchipy.detectors import EBSDDetector
+from kikuchipy.io.plugins._h5ebsd import _hdf5group2dict, H5EBSDReader
 
 
 __all__ = ["file_reader"]
 
 
-class EDAXH5EBSDFile(H5EBSDFile):
-    pass
+class EDAXH5EBSDReader(H5EBSDReader):
+    """EDAX TSL h5ebsd file reader.
+
+    Parameters
+    ----------
+    filename
+        Full file path of the HDF5 file.
+    **kwargs
+        Keyword arguments passed to :class:`h5py.File`.
+    """
+
+    def __init__(self, filename: str, **kwargs):
+        super().__init__(filename, **kwargs)
+
+    def scan2dict(self, group: h5py.Group, lazy: bool = False) -> dict:
+        """Read (possibly lazily) patterns from group.
+
+        Parameters
+        ----------
+        group
+            Group with patterns.
+        lazy
+            Read dataset lazily (default is ``False``).
+
+        Returns
+        -------
+        scan_dict
+            Dictionary with keys ``"axes"``, ``"data"``, ``"metadata"``,
+            ``"original_metadata"``, ``"detector"``,
+            ``"static_background"``, and ``"xmap"``. This dictionary can
+             be passed as keyword arguments to create an
+             :class:`~kikuchipy.signals.EBSD` signal.
+
+        Raises
+        ------
+        IOError
+            If patterns are not acquired in a square grid.
+        """
+        hd = _hdf5group2dict(group["EBSD/Header"], recursive=True)
+        if "SEM-PRIAS Images" in group.keys():
+            sd = _hdf5group2dict(group["SEM-PRIAS Images"])
+        else:
+            sd = {}
+
+        # Ensure file can be read
+        grid_type = hd.get("Grid Type")
+        if grid_type != "SqrGrid":
+            raise IOError(f"Only square grids are supported, not {grid_type}")
+
+        # Get data shapes
+        ny, nx = hd["nRows"], hd["nColumns"]
+        sy, sx = hd["Pattern Height"], hd["Pattern Width"]
+        dy, dx = hd.get("Step Y", 1), hd.get("Step X", 1)
+        px_size = 1.0
+
+        # --- Metadata
+        fname = self.filename.split("/")[-1].split(".")[0]
+        title = fname + " " + group.name[1:].split("/")[0]
+        if len(title) > 20:
+            title = f"{title:.20}..."
+        metadata = {
+            "Acquisition_instrument": {
+                "SEM": {
+                    "working_distance": hd.get("Working Distance"),
+                    "magnification": sd.get("Header", {}).get("Mag"),
+                },
+            },
+            "General": {
+                "authors": hd.get("Operator"),
+                "notes": hd.get("Notes"),
+                "original_filename": fname,
+                "title": title,
+            },
+            "Signal": {"signal_type": "EBSD", "record_by": "image"},
+        }
+        scan_dict = {"metadata": metadata}
+
+        # --- Data
+        # Get HDF5 dataset with pattern array
+        try:
+            data_dset = group["EBSD/Data/" + self.patterns_name]
+        except KeyError:
+            raise KeyError(
+                "Could not find patterns in the expected dataset "
+                f"'EBSD/Data/{self.patterns_name}'"
+            )
+        # Get array from dataset
+        if lazy:
+            if data_dset.chunks is None:
+                chunks = "auto"
+            else:
+                chunks = data_dset.chunks
+            data = da.from_array(data_dset, chunks=chunks)
+        else:
+            data = np.asanyarray(data_dset)
+        # Reshape array
+        try:
+            data = data.reshape((ny, nx, sy, sx)).squeeze()
+        except ValueError:
+            warnings.warn(
+                f"Pattern size ({sx} x {sy}) and scan size ({nx} x {ny}) larger than "
+                "file size. Will attempt to load by zero padding incomplete frames"
+            )
+            # Data is stored image by image
+            pw = [(0, ny * nx * sy * sx - data.size)]
+            if lazy:
+                data = da.pad(data.flatten(), pw)
+            else:
+                data = np.pad(data.flatten(), pw)
+            data = data.reshape((ny, nx, sy, sx))
+        scan_dict["data"] = data
+
+        # --- Axes
+        units = ["um"] * 4
+        scales = np.ones(4)
+        # Calibrate scan dimension and detector dimension
+        scales[0] *= dy
+        scales[1] *= dx
+        scales[2] *= px_size
+        scales[3] *= px_size
+        # Set axes names
+        names = ["y", "x", "dy", "dx"]
+        if data.ndim == 3:
+            if ny > nx:
+                names.remove("x")
+                scales = np.delete(scales, 1)
+            else:
+                names.remove("y")
+                scales = np.delete(scales, 0)
+        elif data.ndim == 2:
+            names = names[2:]
+            scales = scales[2:]
+        # Create list of axis objects
+        scan_dict["axes"] = [
+            {
+                "size": data.shape[i],
+                "index_in_array": i,
+                "name": names[i],
+                "scale": scales[i],
+                "offset": 0.0,
+                "units": units[i],
+            }
+            for i in range(data.ndim)
+        ]
+
+        # --- Original metadata
+        scan_dict["original_metadata"] = {
+            "manufacturer": self.manufacturer,
+            "version": self.version,
+        }
+        scan_dict["original_metadata"].update(hd)
+
+        # --- Crystal map
+        # TODO: Implement reader of EDAX h5ebsd crystal maps in orix
+        xmap = CrystalMap.empty(shape=(ny, nx), step_sizes=(dy, dx))
+        scan_dict["xmap"] = xmap
+
+        # --- Detector
+        scan_dict["detector"] = EBSDDetector(
+            shape=(sy, sx),
+            px_size=px_size,
+            tilt=hd.get("Camera Elevation Angle", 0),
+            azimuthal=hd.get("Camera Azimuthal Angle", 0),
+            sample_tilt=hd.get("Sample Tilt", 70),
+            pc=(
+                hd.get("Pattern Center Calibration", {}).get("x-star", 0.5),
+                hd.get("Pattern Center Calibration", {}).get("y-star", 0.5),
+                hd.get("Pattern Center Calibration", {}).get("z-star", 0.5),
+            ),
+            convention="edax",
+        )
+
+        return scan_dict
 
 
-def file_reader():
-    return
+def file_reader(
+    filename: Union[str, Path],
+    scan_group_names: Union[None, str, List[str]] = None,
+    lazy: bool = False,
+    **kwargs,
+) -> List[dict]:
+    """Read electron backscatter diffraction patterns, a crystal map,
+    and an EBSD detector from an EDAX h5ebsd file
+    :cite:`jackson2014h5ebsd`.
+
+    Parameters
+    ----------
+    filename
+        Full file path of the HDF5 file.
+    scan_group_names
+        Name or a list of names of HDF5 top group(s) containing the
+        scan(s) to return. If not given (default), the first scan in the
+        file is returned.
+    lazy
+        Open the data lazily without actually reading the data from disk
+        until required. Allows opening arbitrary sized datasets. Default
+        is ``False``.
+    **kwargs
+        Keyword arguments passed to :class:`h5py.File`.
+
+    Returns
+    -------
+    scan_dict_list
+        List of one or more dictionaries with the keys ``"axes"``,
+        ``"data"``, ``"metadata"``, ``"original_metadata"``,
+        ``"detector"``, and ``"xmap"``. This
+        dictionary can be passed as keyword arguments to create an
+        :class:`~kikuchipy.signals.EBSD` signal.
+    """
+    reader = EDAXH5EBSDReader(filename, **kwargs)
+    return reader.read(scan_group_names, lazy)
