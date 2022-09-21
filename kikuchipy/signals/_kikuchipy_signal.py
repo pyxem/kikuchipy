e37bda5913bd95727b2897bc773ff73c516670e4
Håkon Wiik Ånes <hwaanes@gmail.com>
2022-09-21 22:06:07 +0200

    Rename private CommonImage->KikuchipySignal2D, better handling of custom properties
    Signed-off-by: Håkon Wiik Ånes <hwaanes@gmail.com>

--- kikuchipy/signals/_kikuchipy_signal.py
+++ kikuchipy/signals/_kikuchipy_signal.py
@@ -15,31 +15,52 @@
 # You should have received a copy of the GNU General Public License
 # along with kikuchipy. If not, see <http://www.gnu.org/licenses/>.
 
-import sys
-from typing import Union, Tuple, Optional
+from copy import deepcopy
+import gc
+import logging
+import os
+from typing import Any, Union, Tuple, Optional
 
+import dask.array as da
 from dask.diagnostics import ProgressBar
 import hyperspy.api as hs
 from hyperspy._signals.signal2d import Signal2D
+from hyperspy._lazy_signals import LazySignal2D
 from hyperspy.misc.rgb_tools import rgb_dtypes
-from hyperspy.api import preferences
 import numpy as np
 from skimage.util.dtype import dtype_range
+import yaml
 
 from kikuchipy.signals.util._dask import get_dask_array
 from kikuchipy.pattern import chunk
 
 
-class CommonImage(Signal2D):
-    """A class extending HyperSpy's Signal2D class with some common
-    intensity manipulation methods.
+_logger = logging.getLogger(__name__)
 
-    Methods inherited from HyperSpy can be found in the HyperSpy user
-    guide.
 
-    See the docstring of :class:`hyperspy.signal.BaseSignal` for a list
-    of attributes.
+# Signal type aliases defined in hyperspy_extension.yaml
+DIR_PATH = os.path.dirname(__file__)
+SIGNAL_TYPES = []
+with open(os.path.join(DIR_PATH, "../hyperspy_extension.yaml")) as f:
+    content = yaml.safe_load(f.read())
+    for _, info in content["signals"].items():
+        for k, v in info.items():
+            if k == "signal_type":
+                SIGNAL_TYPES.append(v)
+            elif k == "signal_type_aliases":
+                SIGNAL_TYPES.extend(v)
+
+
+class KikuchipySignal2D(Signal2D):
+    """General class for image signals in kikuchipy, extending
+    HyperSpy's Signal2D class with some methods for carrying over custom
+    properties and some methods for intensity manipulation.
+
+    Not meant to be used directly, see derived classes like
+    :class:`~kikuchipy.signals.EBSD`.
     """
+
+    _custom_properties = []
 
     def rescale_intensity(
         self,
@@ -92,7 +113,6 @@ class CommonImage(Signal2D):
 
         See Also
         --------
-        kikuchipy.pattern.rescale_intensity,
         :func:`skimage.exposure.rescale_intensity`
 
         Notes
@@ -278,3 +298,126 @@ class CommonImage(Signal2D):
                 pass
         else:
             self.data = normalized_images
+
+    def _get_custom_properties(self) -> dict:
+        """Return a dictionary of properties not in ``Signal2D``.
+
+        This is a quick way to get all custom properties of a class
+        before calling a method of ``Signal2D`` which returns a new
+        instance or operates in place on the current instance and does
+        not carry over these properties, like with ``deepcopy()``.
+
+        Returns
+        -------
+        dictionary
+            Dictionary with custom properties.
+        """
+        return {name: self.__getattribute__(name) for name in self._custom_properties}
+
+    def _set_custom_properties(
+        self,
+        properties: dict,
+        make_deepcopy: bool = False,
+        make_lazy: bool = False,
+        unmake_lazy: bool = False,
+    ):
+        """Set custom properties not in ``Signal2D``.
+
+        This is a quick way to set all custom properties of a class
+        after calling a method of ``Signal2D`` which returns a new
+        instance or operates in place on the current instance and does
+        not carry over these properties, like with ``deepcopy()``.
+
+        Parameters
+        ----------
+        properties
+            Dictionary of custom properties.
+        make_deepcopy
+            Whether to make a deepcopy of all properties before setting
+            them in this instance. Default is ``False``.
+        make_lazy
+            Whether to cast properties which are
+            :class:`~numpy.ndarray` to :class:`~dask.array.Array` before
+            setting them. Default is ``False``.
+        unmake_lazy
+            Whether to cast properties which are
+            :class:`~dask.array.Array` to :class:`~numpy.ndarray` before
+            setting them. Default is ``False``. Ignored if both this and
+            ``make_lazy`` are ``True``.
+        """
+        for name, value in properties.items():
+            if name in self._custom_properties:
+                if make_lazy and isinstance(value, np.ndarray):
+                    value = da.from_array(value)
+                elif unmake_lazy and isinstance(value, da.Array):
+                    value = value.compute()
+                if make_deepcopy:
+                    value = deepcopy(value)
+                self.__setattr__("_" + name, value)
+
+    # --- Inherited methods from Signal2D overwritten
+
+    def as_lazy(self, *args, **kwargs) -> Any:
+        s_new = super().as_lazy(*args, **kwargs)
+
+        if s_new._signal_type in SIGNAL_TYPES:
+            properties = self._get_custom_properties()
+            _logger.debug("Transfer custom properties when making lazy")
+            s_new._set_custom_properties(properties, make_lazy=True)
+
+        return s_new
+
+    def change_dtype(self, *args, **kwargs) -> None:
+        properties = self._get_custom_properties()
+
+        super().change_dtype(*args, **kwargs)
+
+        if self._signal_type in SIGNAL_TYPES:
+            _logger.debug("Transfer custom properties when changing dtype")
+            self._set_custom_properties(properties)
+        else:
+            _logger.debug("Delete custom properties when changing dtype")
+            for name in properties.keys():
+                try:
+                    self.__delattr__("_" + name)
+                except AttributeError:
+                    pass
+
+    def deepcopy(self) -> Any:
+        s_new = super().deepcopy()
+
+        if s_new._signal_type in SIGNAL_TYPES:
+            properties = self._get_custom_properties()
+            _logger.debug("Transfer custom properties when deep copying")
+            s_new._set_custom_properties(properties, make_deepcopy=True)
+
+        return s_new
+
+    def _assign_subclass(self):
+        properties = self._custom_properties
+
+        super()._assign_subclass()
+
+        if self._signal_type not in SIGNAL_TYPES:
+            _logger.debug("Delete custom properties when assigning subclass")
+            for name in properties:
+                try:
+                    self.__delattr__("_" + name)
+                except AttributeError:  # pragma: no cover
+                    pass
+
+
+class LazyKikuchipySignal2D(LazySignal2D, KikuchipySignal2D):
+    """General class for lazy image signals in kikuchipy, extending
+    HyperSpy's LazySignal2D class with some methods for carrying over
+    custom properties and some methods for intensity manipulation.
+
+    Not meant to be used directly, see derived classes like
+    :class:`~kikuchipy.signals.LazyEBSD`.
+    """
+
+    def compute(self, *args, **kwargs) -> None:
+        properties = self._get_custom_properties()
+        super().compute(*args, **kwargs)
+        gc.collect()
+        self._set_custom_properties(properties, unmake_lazy=True)
