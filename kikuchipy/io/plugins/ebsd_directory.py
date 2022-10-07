8f3ddabef51e44bd6fa33932146f5da4aac6a203
Håkon Wiik Ånes <hwaanes@gmail.com>
2022-10-07 21:34:01 +0200

    Add module explanation to new plugin
    Signed-off-by: Håkon Wiik Ånes <hwaanes@gmail.com>

--- kikuchipy/io/plugins/ebsd_directory.py
+++ kikuchipy/io/plugins/ebsd_directory.py
@@ -15,6 +15,8 @@
 # You should have received a copy of the GNU General Public License
 # along with kikuchipy. If not, see <http://www.gnu.org/licenses/>.
 
+"""Reader of EBSD patterns from a dictionary of images."""
+
 import glob
 import logging
 import os
