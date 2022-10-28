b42657e6c4fb1212b61f130944acaa231561736c
Håkon Wiik Ånes <hwaanes@gmail.com>
2022-10-28 12:51:40 +0200

    Add two new pattern processing examples
    Signed-off-by: Håkon Wiik Ånes <hwaanes@gmail.com>

---
+++ examples/pattern_processing/dynamic_background_correction.py
@@ -0,0 +1,33 @@
+"""
+=============================
+Dynamic background correction
+=============================
+
+This example shows how to remove the dynamic background of an EBSD pattern using
+:meth:`~kikuchipy.signals.EBSD.remove_dynamic_background`.
+"""
+
+import matplotlib.pyplot as plt
+import kikuchipy as kp
+
+
+# Load high resolution Si pattern and check that the *static* background
+# pattern is stored with the signal
+s = kp.data.silicon_ebsd_moving_screen_out5mm()
+print(s.static_background)
+
+# Keep original for comparison and remove static and dynamic background
+s.remove_static_background()
+s2 = s.deepcopy()
+s2.dynamic_static_background()
+
+# Plot pattern before and after correction and the intensity histograms
+patterns = [s.data, s2.data]
+fig, axes = plt.subplots(2, 2, height_ratios=[3, 1.5])
+for ax, pattern, title in zip(axes[0], patterns, ["Static", "Dynamic"]):
+    ax.imshow(pattern, cmap="gray")
+    ax.set_title(title)
+    ax.axis("off")
+for ax, pattern in zip(axes[1], patterns):
+    ax.hist(pattern.ravel(), bins=100)
+fig.tight_layout()
