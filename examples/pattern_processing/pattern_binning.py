16af1e81060cd13cb4b9aaedf9dde8451b68a518
Håkon Wiik Ånes <hwaanes@gmail.com>
2022-11-15 20:38:58 -0800

    Split "selecting data" examples into navigation/signal
    Signed-off-by: Håkon Wiik Ånes <hwaanes@gmail.com>

--- examples/pattern_processing/pattern_binning.py
+++ examples/pattern_processing/pattern_binning.py
@@ -3,7 +3,7 @@ Pattern binning
 ===============
 
 This example shows how to bin :class:`~kikuchipy.signals.EBSD` patterns using HyperSpy's
-:meth:`~hyperspy.signal.BaseSignal.rebin`.
+:meth:`~kikuchipy.signals.EBSD.rebin` (see :ref:`hyperspy:rebin-label` for details).
 """
 
 import hyperspy.api as hs
@@ -12,12 +12,16 @@ import numpy as np
 
 
 s = kp.data.silicon_ebsd_moving_screen_in(allow_download=True, show_progressbar=False)
-print(s)
-
 s.remove_static_background(show_progressbar=False)
 
+print(s)
+print(s.static_background.shape)
+print(s.detector)
+
 ########################################################################################
-# Rebin by passing the new shape (use ``(1, 1, 60, 60)`` if binning a 2D map)
+# Rebin by passing the new shape (use ``(1, 1, 60, 60)`` if binning a 2D map). Note how
+# the :attr:`~kikuchipy.signals.EBSD.static_background` and
+# :attr:`~kikuchipy.signals.EBSD.detector` attributes are updated.
 
 s2 = s.rebin(new_shape=(60, 60))
 
@@ -28,22 +32,21 @@ _ = hs.plot.plot_images(
     label=None,
     colorbar=False,
 )
+print(s2.static_background.shape)
+print(s2.detector)
 
 ########################################################################################
 # Rebin by passing the new:old pixel ratio (again, use ``(1, 1, 8, 8)`` for a 2D map)
 
 s3 = s.rebin(scale=(8, 8))
 print(np.allclose(s2.data, s3.data))
+print(s3.static_background.shape)
+print(s3.detector)
 
 ########################################################################################
 # Notice how ``rebin()`` casts the data to ``uint64``, increasing the memory use by a
-# factor of eight (so be careful...)
+# factor of eight (so be careful...). Rescale intensities with
+# :meth:`~kikuchipy.signals.EBSD.rescale_intensity` if desirable.
 
 print(s.data.dtype)
-print(s3.data.dtype)
-
-########################################################################################
-# Rescale intensities to the initial data type
-
-s3.rescale_intensity(dtype_out=s.data.dtype, show_progressbar=False)
 print(s3.data.dtype)
---
+++ examples/selecting_data/crop_signal_axes.py
@@ -0,0 +1,60 @@
+"""
+Crop signal axes
+================
+
+This example shows various ways to crop the signal axes of an
+:class:`~kikuchipy.signals.EBSD` signal using HyperSpy's ``isig`` slicer and the
+:meth:`~kikuchipy.signals.EBSD.crop` and
+:meth:`~hyperspy._signals.signal2d.Signal2D.crop_image` methods (see
+:ref:`hyperspy:signal.indexing` for details).
+"""
+
+import hyperspy.api as hs
+import kikuchipy as kp
+
+
+# Import data
+s = kp.data.nickel_ebsd_small()
+s.remove_static_background(show_progressbar=False)
+
+# Inspect data and attributes
+plot_kwds = dict(axes_decor=None, label=None, colorbar=None, tight_layout=True)
+_ = hs.plot.plot_images(s, **plot_kwds)
+print(s)
+print(s.static_background.shape)
+print(s.detector)
+
+########################################################################################
+# Get a new signal, removing the first and last ten rows of pixels and first and last
+# five columns of pixels. Note how the :attr:`~kikuchipy.signals.EBSD.static_background`
+# and :attr:`~kikuchipy.signals.EBSD.detector` attributes are updated.
+
+s2 = s.isig[5:55, 10:50]
+
+_ = hs.plot.plot_images(s2, **plot_kwds)
+print(s2)
+print(s2.static_background.shape)
+print(s2.detector)
+
+########################################################################################
+# Do the same inplace using :meth:`~kikuchipy.signals.EBSD.crop`
+
+s3 = s.deepcopy()
+s3.crop(2, start=5, end=55)
+s3.crop("dy", start=10, end=50)
+
+_ = hs.plot.plot_images(s3, **plot_kwds)
+print(s3)
+print(s3.static_background.shape)
+print(s3.detector)
+
+########################################################################################
+# Do the same inplace using ``crop_image()``
+
+s4 = s.deepcopy()
+s4.crop_image(top=10, bottom=50, left=5, right=55)
+
+_ = hs.plot.plot_images(s4, **plot_kwds)
+print(s4)
+print(s4.static_background.shape)
+print(s4.detector)
