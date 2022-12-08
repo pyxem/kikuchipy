ed9009cf71a63e10fb695e92a0ea5409acbe149f
Håkon Wiik Ånes <hwaanes@gmail.com>
2022-12-08 22:50:53 +0100

    Remove old refinement setup tests
    Signed-off-by: Håkon Wiik Ånes <hwaanes@gmail.com>

--- kikuchipy/indexing/tests/test_refinement.py
+++ kikuchipy/indexing/tests/test_refinement.py
@@ -23,75 +23,8 @@ import kikuchipy as kp
 
 
 class TestEBSDRefinementSetup:
-    @pytest.mark.parametrize(
-        "method, method_kwargs", [("minimize", None), ("differential_evolution", None)]
-    )
-    def test_get_optimization_method_with_kwargs(self, method, method_kwargs):
-        (
-            method_out,
-            method_kwargs_out,
-        ) = kp.indexing._refinement._refinement._get_optimization_method_with_kwargs(
-            method, method_kwargs
-        )
-        assert callable(method_out)
-        assert method_out.__name__ == method
-        assert isinstance(method_kwargs_out, dict)
-
-        if method == "minimize":
-            assert method_kwargs_out["method"] == "Nelder-Mead"
-
-    def test_get_optimization_method_with_kwargs_raises(self):
-        method = "wait-and-see"
-        with pytest.raises(ValueError, match=f"Method {method} not in the list of"):
-            _ = kp.indexing._refinement._refinement._get_optimization_method_with_kwargs(
-                method
-            )
-
-    def test_check_master_pattern_and_get_data(self):
-        axes = [
-            dict(name="hemisphere", size=2, scale=1),
-            dict(name="energy", size=5, offset=16, scale=1),
-            dict(name="dy", size=5, scale=1),
-            dict(name="dx", size=5, scale=1),
-        ]
-        mp_data = np.random.rand(2, 5, 5, 5).astype(np.float64)
-        mp = kp.signals.EBSDMasterPattern(
-            mp_data,
-            axes=axes,
-            projection="lambert",
-            hemisphere="both",
-            phase=Phase("ni", 225),
-        )
-        (
-            mpn,
-            mps,
-            npx,
-            npy,
-            scale,
-        ) = kp.indexing._refinement._refinement._get_master_pattern_data(
-            master_pattern=mp,
-            energy=20,
-        )
-
-        assert npx == mp_data.shape[3]
-        assert npy == mp_data.shape[2]
-        assert scale == (mp_data.shape[3] - 1) / 2
-
-        # Master patterns are rescaled
-        assert mpn.dtype == np.float32
-        assert not np.allclose(mpn, mp_data[0, -1])
-        assert not np.allclose(mps, mp_data[1, -1])
-        assert np.min(mpn) >= -1
-        assert np.max(mpn) <= 1
+    pass
 
 
 class TestRefinementSolverNumba:
-    def test_mask_pattern_numba(self):
-        p = np.arange(100).astype("float32")
-        mask = np.ones(p.size, dtype=bool)
-        mask[:10] = False
-        p_masked = kp.indexing._refinement._solvers._mask_pattern(p, mask)
-        p_masked2 = kp.indexing._refinement._solvers._mask_pattern.py_func(p, mask)
-        assert p_masked.size == 90
-        assert np.isclose(p_masked.mean(), 54.5)
-        assert np.allclose(p_masked, p_masked2)
+    pass
