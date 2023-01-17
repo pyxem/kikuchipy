37f44a47aa5cd663dc949e290a66f9a6dc4b08be
Håkon Wiik Ånes <hwaanes@gmail.com>
2023-01-17 14:53:12 +0100

    Improve check of crystal map before EBSD refinement
    Signed-off-by: Håkon Wiik Ånes <hwaanes@gmail.com>

--- kikuchipy/signals/util/_crystal_map.py
+++ kikuchipy/signals/util/_crystal_map.py
@@ -19,13 +19,14 @@
 and an :class:`~kikuchipy.signals.EBSD` signal.
 """
 
+from typing import Optional, Tuple
 import warnings
 
 import numpy as np
-from orix.crystal_map import CrystalMap
+from orix.crystal_map import CrystalMap, Phase
 
 
-def _crystal_map_is_compatible_with_signal(
+def _xmap_is_compatible_with_signal(
     xmap: CrystalMap, navigation_axes: tuple, raise_if_not: bool = False
 ) -> bool:
     """Check whether a signal's navigation axes are compatible with a
@@ -38,23 +39,71 @@ def _crystal_map_is_compatible_with_signal(
         xmap_scale = list([xmap._step_sizes[a.name] for a in navigation_axes])
     except KeyError:
         warnings.warn(
-            "The signal navigation axes must be named 'x' and/or 'y' in order to "
-            "compare the signal navigation scale to the CrystalMap step sizes 'dx' and "
-            "'dy' (see `EBSD.axes_manager`)"
+            "Signal navigation axes must be named 'x' and/or 'y' in order to compare "
+            "the signal navigation scales to the crystal map step sizes 'dx' and 'dy' "
+            "(see `EBSD.axes_manager`)"
         )
         xmap_scale = list(xmap._step_sizes.values())[-len(navigation_axes) :]
 
     compatible = xmap.shape == nav_shape
     if compatible and not np.allclose(xmap_scale, nav_scale, atol=1e-6):
         warnings.warn(
-            f"The `xmap` step size(s) {xmap_scale} are different from the signal's "
-            f"step size(s) {nav_scale} (see `EBSD.axes_manager`)"
+            f"Crystal map step size(s) {xmap_scale} and signal's step size(s) "
+            f"{nav_scale} must be the same (see `EBSD.axes_manager`)"
         )
+
     if not compatible and raise_if_not:
         raise ValueError(
-            f"The `xmap` shape {xmap.shape} and step size(s) {xmap_scale} are not "
-            f"compatible with the signal navigation shape {nav_shape} and step size(s) "
-            f"{nav_scale} (see `EBSD.axes_manager`)"
+            f"Crystal map shape {xmap.shape} and signal's navigation shape {nav_shape} "
+            "must be the same (see `EBSD.axes_manager`)"
         )
     else:
         return compatible
+
+
+# TODO: Move to orix' Phase.__eq__
+def _equal_phase(phase1: Phase, phase2: Phase) -> bool:
+    try:
+        equal_sg = phase1.space_group.number == phase2.space_group.number
+    except AttributeError:
+        equal_sg = True
+    equal_pg = phase1.point_group == phase2.point_group
+    equal_structure = len(phase1.structure) == len(phase2.structure)
+    if equal_structure:
+        for atom1, atom2 in zip(phase1.structure, phase2.structure):
+            equal_structure *= atom1.element == atom2.element
+            equal_structure *= np.allclose(atom1.xyz, atom2.xyz)
+            equal_structure *= np.isclose(atom1.occupancy, atom2.occupancy)
+            if not equal_structure:
+                break
+    return bool(equal_sg * equal_pg * equal_structure)
+
+
+def _get_points_in_data_in_xmap(
+    xmap: CrystalMap,
+    navigation_mask: Optional[np.ndarray] = None,
+) -> Tuple[np.ndarray, int]:
+    points_to_refine = xmap.is_in_data
+
+    if navigation_mask is not None:
+        nav_mask_shape = navigation_mask.shape
+        if navigation_mask.shape != xmap.shape:
+            raise ValueError(
+                f"Navigation mask shape {nav_mask_shape} and crystal map shape "
+                f"{xmap.shape} must be the same"
+            )
+        points_to_refine_in_mask = ~navigation_mask.ravel()
+        points_to_refine_in_data = points_to_refine_in_mask[points_to_refine]
+        points_to_refine = np.logical_and(points_to_refine, points_to_refine_in_mask)
+        phase_id = np.unique(xmap.phase_id[points_to_refine_in_data])
+    else:
+        phase_id = np.unique(xmap.phase_id)
+
+    if phase_id.size != 1:
+        raise ValueError(
+            "Points to refine in crystal map must have only one phase, but had the "
+            f"phase IDs {list(phase_id)}"
+        )
+    unique_phase_id = phase_id[0]
+
+    return points_to_refine, unique_phase_id
