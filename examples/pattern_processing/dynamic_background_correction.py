c6d29944e5d94a17301e91b301954c53149e1cb1
Håkon Wiik Ånes <hwaanes@gmail.com>
2026-03-21 15:44:15 +0100

    Set tight layout of Matplotlib figure upon creation instead of afterwards
    Signed-off-by: Håkon Wiik Ånes <hwaanes@gmail.com>

--- examples/pattern_processing/dynamic_background_correction.py
+++ examples/pattern_processing/dynamic_background_correction.py
@@ -1,3 +1,21 @@
+#
+# Copyright 2019-2026 the kikuchipy developers
+#
+# This file is part of kikuchipy.
+#
+# kikuchipy is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# kikuchipy is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with kikuchipy. If not, see <http://www.gnu.org/licenses/>.
+#
 """
 =============================
 Dynamic background correction
@@ -10,10 +28,13 @@ More details are given in the
 :doc:`pattern processing tutorial </tutorials/pattern_processing>`.
 """
 
+# %%
+# Imports.
 import matplotlib.pyplot as plt
 
 import kikuchipy as kp
 
+# %%
 # Load low resolution Ni patterns and check that the *static* background
 # pattern is stored with the signal
 s = kp.data.nickel_ebsd_small()
@@ -22,13 +43,13 @@ print(s.static_background)
 s.remove_static_background()
 s2 = s.remove_dynamic_background(inplace=False)
 
+# %%
 # Plot pattern before and after correction and the intensity histograms
 patterns = [s.inav[0, 0].data, s2.inav[0, 0].data]
-fig, axes = plt.subplots(2, 2, height_ratios=[3, 1.5])
+fig, axes = plt.subplots(2, 2, height_ratios=[3, 1.5], layout="tight")
 for ax, pattern, title in zip(axes[0], patterns, ["Static", "Dynamic"]):
     ax.imshow(pattern, cmap="gray")
     ax.set_title(title)
     ax.axis("off")
 for ax, pattern in zip(axes[1], patterns):
     ax.hist(pattern.ravel(), bins=100)
-fig.tight_layout()
--- examples/visualization/plot_nice_master_pattern_image.py
+++ examples/visualization/plot_nice_master_pattern_image.py
@@ -1,3 +1,21 @@
+#
+# Copyright 2019-2026 the kikuchipy developers
+#
+# This file is part of kikuchipy.
+#
+# kikuchipy is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# kikuchipy is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with kikuchipy. If not, see <http://www.gnu.org/licenses/>.
+#
 """
 Plot nice master pattern image
 ==============================
@@ -7,26 +25,31 @@ More details are given in the
 :doc:`visualization tutorial </tutorials/visualizing_patterns>`.
 """
 
+# %%
+# Imports.
 import matplotlib.pyplot as plt
 import numpy as np
 
 import kikuchipy as kp
 
-# Load both hemispheres of master pattern in stereographic projection
+# %%
+# Load both hemispheres of master pattern in stereographic projection.
 mp = kp.data.nickel_ebsd_master_pattern_small(hemisphere="both")
+print(mp)
 
+# %%
 # Extract the underlying data of both hemipsheres and mask out the
-# surrounding black pixels
+# surrounding black pixels.
 data = mp.data.astype("float32")
 mask = data[0] == 0
 data[:, mask] = np.nan
 
-# Plot both hemispheres with labels
-fig, (ax0, ax1) = plt.subplots(ncols=2)
+# %%
+# Plot both hemispheres with labels.
+fig, (ax0, ax1) = plt.subplots(ncols=2, layout="tight")
 ax0.imshow(data[0], cmap="gray")
 ax1.imshow(data[1], cmap="gray")
 ax0.axis("off")
 ax1.axis("off")
 ax0.set_title("Upper")
 ax1.set_title("Lower")
-fig.tight_layout()
